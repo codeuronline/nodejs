f2d55884e512109be0cd9f11911d22863f92c1ab
codeuronline <j.kasperski@codeur.online>
2022-06-22 09:59:53 +0200

    update -> etape vue

--- node-express-app/app.js
+++ node-express-app/app.js
@@ -1,15 +1,14 @@
 
-//const express = require('express');
+// const express = require('express');
 const connexion = require('connexion');
 const creation = require('create');
-
-//const monmodule = require('monmodule');
+const monmodule = require('monmodule');
 const route = require('route');
-const app = route;
+// const app = express();
 // app.listen(5050, () => {
-//     console.log('Server is listening @5050');
-// });
-// app.get('/hello', function (request, response) {
-//     monmodule.direBonjour();
-//     monmodule.direBye();
-// })
+//      console.log('Server is listening @5050');
+//  });
+//  app.get('/hello', function (request, response) {
+//      route.postResto;
+//      monmodule.direBye();
+//  })
--- node-express-app/index.js
+++ node-express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 let mysql = require('mysql');
 console.log('Get connection ...');
 const app = express();
-app.listen(5050, () => {
+app.listen(5000, () => {
     console.log('Server is listening @5000');
 });
 
