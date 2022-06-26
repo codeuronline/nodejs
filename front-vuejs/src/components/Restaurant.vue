75fe0dd2113c37195a5f27eb7057d57099db3763
codeuronline <j.kasperski@codeur.online>
2022-06-26 16:54:32 +0200

    update main

--- front-vuejs/src/components/Restaurant.vue
+++ front-vuejs/src/components/Restaurant.vue
@@ -1,16 +1,16 @@
 <template>
-    <div id="app">
-        <h1>formulaire d'ajout d'un Restaurant</h1>
-    </div>
+  <div id="app">
+    <h1>formulaire d'ajout d'un Restaurant</h1>
+  </div>
 </template>
 <script>
 export default {
-    name: "app",
-    data() {
-        return {
-            restaurants: null,
-            team: null,
-        };
-    },
+  name: "app",
+  data() {
+    return {
+      restaurants: null,
+      team: null,
+    };
+  },
 };
 </script>
