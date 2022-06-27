44104405fffaa808fffceb69eb064749459ca3fd
PCGameJK <j.kasperski@codeur.online>
2022-06-28 00:05:33 +0200

    update control ok on lastname/firstname

--- front-vuejs/src/views/EmployeView.vue
+++ front-vuejs/src/views/EmployeView.vue
@@ -1,66 +1,66 @@
 /* eslint-disable */
 <template>
   <div class="about">
-    <h1>Ajout d'un employé</h1>
-    <div class="alert alert-warning">
-      <h2><span id="information"></span></h2>
-    </div>
-    <form @submit="postData" method="post">
-      <fieldset>
-        <legend>[Identité]</legend>
-        <input
-          type="text"
-          name="last_name"
-          id="last_name"
-          placeholder="Nom"
-          v-model="posts.last_name"
-          @keyup="verifL"
-        />
+    <div class="container">
+      <h1>Ajout d'un employé</h1>
+      <span id="information" class="alert alert-warning"></span>
+      <form @submit="postData" method="post">
+        <fieldset>
+          <legend>Identité</legend>
+          <label for="last_name"> Nom :</label>
+          <input
+            type="text"
+            name="last_name"
+            id="last_name"
+            v-model="posts.last_name"
+            @keyup="verifLast"
+          />
+          <br />
+          <label for="first_name"> Prénom :</label>
+          <input
+            type="text"
+            name="first_name"
+            id="first_name"
+            v-model="posts.first_name"
+            @keyup="verifFirst"
+          />
+          <br />
+        </fieldset>
         <br />
-        <input
-          type="text"
-          name="first_name"
-          id="first_name"
-          placeholder="Prénom"
-          v-model="posts.first_name"
-          @keypup="verifF"
-        />
-        <br />
-      </fieldset>
-      <br />
-      <fieldset>
-        <legend>[Date d'embauche ]</legend>
-        <input
-          type="date"
-          name="hire_date"
-          id="hire_date"
-          v-model="posts.hire_date"
-        />
-      </fieldset>
-      <fieldset>
-        <legend>[Restaurant]</legend>
-        <select
-          name="restaurant_id"
-          id="restaurant_id"
-          v-model="posts.restaurant_id"
-        >
-          <option
-            v-for="(restaurant, index) in restaurants"
-            v-bind:key="index"
-            :value="restaurant.id_restaurant"
+        <fieldset>
+          <legend>Information(s)</legend>
+          <label for="hire_date">Date d'embauche : </label>
+          <input
+            type="date"
+            name="hire_date"
+            id="hire_date"
+            v-model="posts.hire_date"
+          />
+          <label for="restaurant_id"> dans le Restaurant : </label>
+          <select
+            name="restaurant_id"
+            id="restaurant_id"
+            v-model="posts.restaurant_id"
           >
-            {{ restaurant.name }}
-          </option>
-        </select>
-      </fieldset>
-      <button id="valider" type="submit">Valider</button>
-      &nbsp;&nbsp;
-      <button type="cancel">Annuler</button>
-    </form>
+            <option value="" :select="selected">
+              Séléctionner le Restaurant
+            </option>
+            <option
+              v-for="(restaurant, id_restaurant) in restaurants"
+              v-bind:key="id_restaurant"
+              value="restaurant.id_restaurant"
+            >
+              {{ restaurant.name }}
+            </option>
+          </select>
+        </fieldset>
+        <button id="button" type="submit">Valider</button> |
+        <button type="cancel">Annuler</button>
+      </form>
+    </div>
   </div>
 </template>
 <script>
-/* eslint-disable */
 import axios from "axios";
 export default {
   name: "PostComponent",
@@ -70,63 +70,47 @@ export default {
         first_name: null,
         last_name: null,
         hire_date: null,
-        restaurant_id: "",
+        restaurant_id: null,
         id_restaurant: null,
       },
       restaurants: {
-        name: "",
+        name: null,
         city: null,
-        id_restaurant: "",
+        id_restaurant: null,
         // nbCouverts: null,
-        // null, parking: null,
+        // parking: null,
         // terrasse: null,
       },
     };
   },
-
   methods: {
-    verifL(e) {
-      //&& (document.getElementById("last_name").value.match(/^[a-zA-Z]+$/))) {
-      //  cas des 3 caracteres
-      let ln = document.getElementById("last_name").value
-      console.log(ln)
-         if (ln.match(/^[a-zA-Z][a-zA-Z][a-zA-Z]+$/)) {
-          document.getElementById("information").innerHTML = "";
-          document.querySelector("button").disabled = false;
-         } else {
-           document.getElementById("information").innerHTML = "Le nom doit contenir QUE des caractères";
-           document.querySelector("button").disabled = true;
-         }
-            
-        e.preventDefault();
-      
+    verifLast(e) {
+      let ln = document.getElementById("last_name").value;
+      console.log(ln);
+      if (ln.match(/^[a-zA-Z][a-zA-Z][a-zA-Z]+$/)) {
+        document.getElementById("button").disabled = false;
+        document.getElementById("information").innerHTML = "";
+      } else {
+        document.getElementById("button").disabled = true;
+        document.getElementById("information").innerHTML =
+          "le nom doit contenir que des caractères";
+      }
+      e.preventDefault();
     },
-    verifF(e) {
-      //&& (document.getElementById("last_name").value.match(/^[a-zA-Z]+$/))) {
-      //  cas des 3 caracteres
-    // let fn = document.getElementById("last_name").value
-    //   if (fn.match(/^[a-zA-Z][a-zA-Z][a-zA-Z]+$/)) {
-    //       document.getElementById("information").innerHTML = "";
-    //       document.querySelector("button").disabled = false;
-    //      } else {
-    //        document.getElementById("information").innerHTML = "Le nom doit contenir QUE des caractères";
-    //        document.querySelector("button").disabled = true;
-    //      }
-    //     // if (document.getElementById("last_name").value.match(/^[a-zA-Z]+$/)) {
-        //   document.getElementById("information").innerHTML = "";
-        //   document.querySelector("button").disabled = false;
-        // } else {
-        //   document.getElementById("information").innerHTML = "Le nom ET le prénom doivent contenir QUE des caractères";
-        //   document.querySelector("button").disabled = true;
-        // }
-        e.preventDefault();
-      
+    verifFirst(e) {
+      let fn = document.getElementById("first_name").value;
+      console.log(fn);
+      if (fn.match(/^[a-zA-Z][a-zA-Z][a-zA-Z]+$/)) {
+        document.getElementById("button").disabled = false;
+        document.getElementById("information").innerHTML = "";
+      } else {
+        document.getElementById("button").disabled = true;
+        document.getElementById("information").innerHTML =
+          "le prénom doit contenir que des caractères";
+      }
+      e.preventDefault();
     },
-
     postData(e) {
-      // if (this.posts.hire_date == null) {
-      //   this.posts.hire_date = dateFormat(new Date(), "YYYY-MM-DD");
-      // }
       axios
         .post("http://127.0.0.1:5000/employe/", this.posts)
         .then((result) => {
@@ -136,7 +120,6 @@ export default {
     },
   },
   mounted() {
-    // document.querySelector("button").disabled = true;
     axios
       .get("http://127.0.0.1:5000/restaurants")
       .then((res) => (this.restaurants = res.data));
