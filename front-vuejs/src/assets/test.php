02ee27fad74fd5c06adbd28e79ed06f1601451e1
codeuronline <j.kasperski@codeur.online>
2022-07-07 09:32:45 +0200

    update -> all work appli finished

--- front-vuejs/src/assets/test.php
+++ front-vuejs/src/assets/test.php
@@ -3,11 +3,8 @@ $people = array(
 array("name"=>"Bob","age"=>8,"colour"=>"red"),
 array("name"=>"Greg","age"=>12,"colour"=>"blue"),
 array("name"=>"Andy","age"=>15,"colour"=>"purple"));
-
 var_dump($people);
-
 $sortArray = array();
-
 foreach($people as $person) {
     foreach($person as $key=>$value) {
         if(!isset($sortArray[$key])) {
@@ -17,12 +14,10 @@ foreach($people as $person) {
     }
 } 
 $orderby = "name"; //change this to whatever key you want from the array
-
-//array_multisort($sortArray[$orderby],SORT_ASC,$people);
+array_multisort($sortArray[$orderby],SORT_ASC,$people);
 //array_multisort($people,$sortArray);
 //array_multisort($sortArray["name"],$people);
 array_multisort($sortArray["name"],$sortArray["age"],$people);
-
 var_dump($people);
 var_dump($sortArray);
 var_dump($orderby);
